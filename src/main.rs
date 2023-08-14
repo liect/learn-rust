8edca59855dc0e0261381344f57ac7683a394588
“Aicmortal” <aicmortal@gmail.com>
2023-08-14 10:08:15 +0800

    feat: for loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    let a:u8 = 255;
+    let b = a.saturating_add(255);
+    println!("a = {}", a);
+    println!("b = {}", b);
+    for i in 1..=5 {
+        println!("i = {}", i);
+    }
+    for i in 'a'..='z' {
+        println!("i = {}", i);
+        
+    }
 }
