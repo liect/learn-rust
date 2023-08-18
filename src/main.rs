8483484dce00b174f3ba1dabb51f5f2e84d8bcf7
“Aicmortal” <aicmortal@gmail.com>
2023-08-18 17:00:37 +0800

    feat: learn trait

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,71 @@
 trait Animal {
-    fn name(&self);
+    type Output;
+    fn name(&self) -> Self::Output;
+    fn say_bye(&self) {
+        println!("Bye");
+    }
 }
 
+#[derive(Debug)]
 struct Human {
-    name:String
+    name: String,
 }
 
 struct Cat {
-    name:String
+    name: String,
 }
 
 impl Animal for Human {
-    fn name(&self) {
+    type Output = u32;
+    fn name(&self) -> u32 {
         println!("Human name is {}", self.name);
+        1
     }
 }
 
 impl Animal for Cat {
-    fn name(&self) {
+    type Output = u32;
+    fn name(&self) -> u32 {
         println!("Cat name is {}", self.name);
+        2
     }
 }
 
 fn say_hello<T: Animal>(animal: T) {
     animal.name();
 }
-    
+
+fn new_animal(is_cat: bool) -> Box<dyn Animal<Output = u32>> {
+    if is_cat {
+        Box::new(Cat {
+            name: "Tom".to_string(),
+        })
+    } else {
+        Box::new(Human {
+            name: "John".to_string(),
+        })
+    }
+}
 
 fn main() {
+    let human = Human {
+        name: "John".to_string(),
+    };
 
-    let human = Human { name: "John".to_string() };
-    let cat = Cat { name: "Tom".to_string() };
+    let v = vec![1, 2, 3];
+    println!("v = {:?}", v);
+    let mut v2 = Vec::new();
+    v2.push(Box::new(human));
+    let t1 = &v2[0];
 
-    say_hello(human);
-    say_hello(cat);
+    println!("t1 = {:?}", v2[0]);
+
+    let v3 = vec![Box::new(Cat {
+        name: "Tom".to_string(),
+    })];
+
+    // say_hello(cat);
+    let a = new_animal(true).name();
+    let b = new_animal(false).name();
+    println!("a = {}, b = {}", a, b)
 }
